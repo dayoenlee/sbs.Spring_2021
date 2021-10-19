4deb5963a8d4efd5bdaad36a992133ae327ad850
dayoenlee <dlekdus33483@gmail.com>
2021-10-19 16:53:44 +0900

    writeArticle 메서드를 이용해 중복제거

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -29,27 +29,28 @@ public class UsrArticleController {
 	
 	private void makeTestData() {
 		for(int i =1; i <=10; i ++) {
-			int id =articleLastId +1;
-			
-			String title ="제목"+i;
-			String body = "내용"+i;
-			Article article = new Article (id,title,body);
-			
-			articles.add(article);
-			articleLastId = id;
+			String title ="제목"+ i;
+			String body ="내용"+ i;
+			writeArticle(title,body);
 		}
 		
+	}
+	
+	public Article writeArticle(String title,String body) {
+		int id =articleLastId +1;
+		Article article = new Article (id,title,body);
+		
+		articles.add(article);
+		articleLastId = id;
+		
+		return article;
 	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title,String body) {
+		Article article = writeArticle(title,body);
 		
-		int id = articleLastId +1;
-		Article article = new Article (id,title,body);
-		
-		articles.add(article);
-		articleLastId = id;
 		
 		return article;
 	}
