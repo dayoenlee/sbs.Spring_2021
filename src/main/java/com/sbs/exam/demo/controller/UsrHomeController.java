b269bd8b0e22dcbc92b209817c41bdc6b0a4ceac
dayoenlee <dlekdus33483@gmail.com>
2021-10-19 13:36:06 +0900

    세팅

---
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -0,0 +1,31 @@
+package com.sbs.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+	static int count = 0;
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요";
+	}
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다";
+	}
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "잘가세요";
+	}
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		count++;
+		return count;
+	}
+}	
