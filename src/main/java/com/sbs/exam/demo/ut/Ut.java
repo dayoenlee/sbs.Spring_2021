d23bef32b68ab62bfedab12ba4067c640ee28891
dayoenlee <dlekdus33483@gmail.com>
2021-10-31 17:18:16 +0900

    Ut.empty 도입

---
+++ src/main/java/com/sbs/exam/demo/ut/Ut.java
@@ -0,0 +1,19 @@
+package com.sbs.exam.demo.ut;
+
+public class Ut {
+
+	public static boolean empty(Object obj) {
+		
+		if (obj == null) {
+			return true;
+		}
+		//instanceof 객체타입을 확인하는 연산자 String 이냐 맞으면 true 틀리면false
+		if(obj instanceof String == false) {
+			return true;
+		}
+		String str = (String) obj;
+		
+		return str.trim().length() == 0;
+	}
+
+}
